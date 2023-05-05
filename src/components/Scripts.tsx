039881a12ff4291ecbeb41ea9bae98e1afb87318
Saltys-Git <sidurrahmansiam@gmail.com>
2023-05-06 04:35:03 +0600

    eslint changed

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -1,7 +1,16 @@
+'use client'
+import Script from 'next/script'
+
 const Scripts = ({link}: any) => {
+
+    const link2 = 'https://techys.repairdesk.co/widgets/repair_widget.js?token=67388641edd1478dd511679744276&width=100%&scrolling=no';
+
     return (
         <>
-            <script id="repairdeskwidget" src={link}/>
+            {/*<Script strategy="lazyOnload" async={true} id="repairdeskwidget" src='https://techys.repairdesk.co/widgets/repair_widget.js?token=67388641edd1478dd511679744276&width=100%&scrolling=no' onLoad={() => {
+                console.log('Script has loaded');
+            }}></Script>*/}
+            <script async={false} id='repairdeskwidget' src={link}></script>
         </>
     )
 }
